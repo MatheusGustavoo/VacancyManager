fbd311c790118fed78fe7ccb94b55d06fbaa6192
Matheus_Note <matheussgustavoo@gmail.com>
2026-01-15 14:07:46 -0300

    feat: add candidate validation

--- src/main/java/com/example/management_system/modules/candidate/Candidate.java
+++ src/main/java/com/example/management_system/modules/candidate/Candidate.java
@@ -1,15 +1,27 @@
 package com.example.management_system.modules.candidate;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class Candidate {
+    
+    @Id
+    @Pattern(regexp = "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})", message = "CPF inválido. Digite um CPF no formato: 'xxx.xxx.xxx-xx' ou 'xxx.xxx.xxx/xxxx-xx'")
+    @NotBlank
     private String CPF;
     
+    @NotBlank
     @Pattern(regexp = "^[a-zA-Z ]+$", message = "O nome deve conter apenas letras e espaços")
     private String name;
     
@@ -21,4 +33,6 @@ public class Candidate {
     private String description;
     private String resume;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/example/management_system/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/example/management_system/modules/candidate/controller/CandidateController.java
@@ -2,10 +2,13 @@ package com.example.management_system.modules.candidate.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.management_system.modules.candidate.Candidate;
+import com.example.management_system.modules.candidate.CandidateRepository;
 
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -15,8 +18,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 public class CandidateController {
     
+    @Autowired
+    private CandidateRepository repository;
+
     @PostMapping("/")
-    public void candidate(@Valid @RequestBody Candidate candidate){ 
-        
+    public Candidate create(@Valid @RequestBody Candidate candidate){ 
+        this.repository.findByCPFOrEmail(candidate.getCPF(), candidate.getEmail())
+        return this.repository.save(candidate);
+    }
+
+    @GetMapping("/")
+    public String findAll(){
+        return "test";
     }
 }
